420eda8a835bc768bf805d53fa7e740c49b37b92
kburts <kevin.burton2@gmail.com>
2023-12-21 23:47:45 -0800

    First commit 20 or so katas

---
+++ exercises/variables/variables1.rs
@@ -0,0 +1,11 @@
+// variables1.rs
+//
+// Make me compile!
+//
+// Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
+// hint.
+
+fn main() {
+    let x: i32 = 10;
+    println!("x has the value {}", x);
+}
